157b11ef09f66c50166231a3c4c6cf0db23a4863
Jeff Clegg <jeffclegg@att.net>
2021-03-06 18:25:10 -0500

    preparing for deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
