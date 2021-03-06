ad13323d9c33636d451ada123e1bcc59883928bc
Jeff Clegg <jeffclegg@att.net>
2021-03-06 18:03:12 -0500

    added Login

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client"));
 }
 // Add routes, both API and view
 app.use(routes);
