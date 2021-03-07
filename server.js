d817014e42263aa99da2676a820e241ab1b85c33
Jeff Clegg <jeffclegg@att.net>
2021-03-06 20:10:08 -0500

    fixed connection

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(routes);
 // app.use('/api/user', authRoutes);
 
 //URI connection to Atlas Db
-const uri = process.env.MONGODB_URI || "mongodb://localhost/robotSolutions";
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
